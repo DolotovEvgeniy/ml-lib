922e6648d1c11e5ae4cc06c91957557e3ad29646
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2016-05-22 01:24:19 +0400

    fix travis

--- include/hello_world.h
+++ include/hello_world.h
@@ -2,5 +2,5 @@
 
 #ifndef INCLUDE_HELLO_WORLD_H_
 #define INCLUDE_HELLO_WORLD_H_
-
+float sum(float a, float b);
 #endif  // INCLUDE_HELLO_WORLD_H_
--- test/test_hello_wrold.cpp
+++ test/test_hello_wrold.cpp
@@ -1,8 +1,7 @@
 // Copyright 2016 Dolotov Evgeniy
 
 #include <gtest/gtest.h>
-
+#include "../include/hello_world.h"
 TEST(ML, Can_ML) {
-    bool is = true;
-    EXPECT_EQ(true, is);
+    EXPECT_EQ(10, sum(5,5));
 }
