9a5ed8d9f1b52d509a9eb453b5e3c0f18870e09e
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2016-06-03 21:02:15 +0400

    Initial implementation linear algebra

--- include/hello_world.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2016 Dolotov Evgeniy
-
-#ifndef INCLUDE_HELLO_WORLD_H_
-#define INCLUDE_HELLO_WORLD_H_
-float sum(float a, float b);
-#endif  // INCLUDE_HELLO_WORLD_H_
--- test/test_hello_wrold.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 2016 Dolotov Evgeniy
-
-#include <gtest/gtest.h>
-#include "../include/hello_world.h"
-TEST(ML, Can_ML) {
-    EXPECT_EQ(10, sum(5,5));
-}
